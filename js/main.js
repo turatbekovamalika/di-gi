5417c8c63783ecc4cafa51372cec41c36d6e87ef
Turatbekova Malika <m18049687>
2025-11-16 20:54:42 +0600

    add portrait first

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ $(function () {
 
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow:
+      '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="arrow"></button>',
+    prevArrow:
+      '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="arrow"></button>',
     responsive: [
       {
         breakpoint: 1024,
